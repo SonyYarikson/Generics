f46a0d94eb2cd4b82641cf0b0f2418463004691c
yaros <yaros@LAPTOP-FJ2D97RD>
2023-05-13 14:46:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public class MyList<T>
+        {
+            public T Row1 { get; set; }
+            public T Row2 { get; set; }
+            public MyList<T> List { get; set; } = null;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ку");
+            var node = new MyList<string>();
+        }
+    }
+}
